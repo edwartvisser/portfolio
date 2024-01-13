813bb4c789b6338af1aafe986ca6f07c9b34752b
edwartvisser <flashpro@gmail.com>
2024-01-13 19:49:21 +0100

    outline hover effect

--- components/card.tsx
+++ components/card.tsx
@@ -10,7 +10,7 @@ type CardProps = {
 
 const Card: React.FC<CardProps> = ({ title, description }) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden min-h-64">
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden min-h-64 hover:outline outline-offset-2 outline-blue-400 outline-1">
         <div className="p-4">
             <h3 className="font-bold text-lg">{title}</h3>
             <p className="text-gray-600">{description}.</p>
